5a225645363e58e3b02d36e7e6ae6f11469aa321
puha <sys88515170@gmail.com>
2019-12-22 00:21:00 +0000

    some unit test

--- TeaTests/UnitTest1.cs
+++ TeaTests/UnitTest1.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tea;
 
 namespace TeaTests
 {
     [TestClass]
-    public class UnitTestTea
+    public class GeoUtilsTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        GeoPosition Dublin = new GeoPosition
         {
+            LatitudeInRadians = (double)53.339428.ToRadians(),
+            LongitudeInRadians = (double)-6.257664.ToRadians()
+        };
+
+        [TestMethod]
+        public void TestCalcDistanceInKm_DublinIsCloserToBarcelonaThanToBeijing()
+        {
+            GeoPosition Beijing = new GeoPosition
+            {
+                LatitudeInRadians = (double)39.895362.ToRadians(),
+                LongitudeInRadians = (double)116.357594.ToRadians()
+            };
+
+            GeoPosition Barcelona = new GeoPosition
+            {
+
+                LatitudeInRadians = (double)41.387042.ToRadians(),
+                LongitudeInRadians = (double)2.152542.ToRadians()
+            };
+
+            Assert.IsTrue(GeoUtils.CalcDistanceInKm(Dublin, Barcelona) < GeoUtils.CalcDistanceInKm(Dublin, Beijing));
+                
         }
     }
 }
